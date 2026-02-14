82b374ad3149e746c16ef3cddd91e3fef1258e96
Anthony Brown <anthony.brown8@nhs.net>
2026-02-14 15:58:01 +0000

    fix setting version

--- src/languages/node_24_python_3_14/.devcontainer/scripts/root_install.sh
+++ src/languages/node_24_python_3_14/.devcontainer/scripts/root_install.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
 set -e
-
-# store version info in VERSION.txt for reference
-echo "VERSION=${BASE_VERSION}" > "${SCRIPTS_DIR}/VERSION.txt"
-echo "CONTAINER_NAME=${CONTAINER_NAME}" >> "${SCRIPTS_DIR}/VERSION.txt"
