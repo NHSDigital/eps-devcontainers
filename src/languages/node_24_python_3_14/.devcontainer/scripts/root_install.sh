22a47f1b88e259a00edb1bc3ddbdb4bb303beeea
Anthony Brown <anthony.brown8@nhs.net>
2026-02-14 15:17:05 +0000

    set correct user

--- src/languages/node_24_python_3_14/.devcontainer/scripts/root_install.sh
+++ src/languages/node_24_python_3_14/.devcontainer/scripts/root_install.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 set -e
+
+# store version info in VERSION.txt for reference
+echo "VERSION=${BASE_VERSION}" > "${SCRIPTS_DIR}/VERSION.txt"
+echo "CONTAINER_NAME=${CONTAINER_NAME}" >> "${SCRIPTS_DIR}/VERSION.txt"
