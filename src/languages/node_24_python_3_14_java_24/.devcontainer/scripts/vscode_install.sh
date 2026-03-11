c82b4269504f3335c461d539dddd2042ccc32137
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2026-03-11 12:15:21 +0000

    Chore: [AEA-0000] - create java image (#50)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - create a java image

---
+++ src/languages/node_24_python_3_14_java_24/.devcontainer/scripts/vscode_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+asdf plugin add python
+asdf plugin add poetry https://github.com/asdf-community/asdf-poetry.git
+asdf plugin add java
+asdf plugin add maven
+
+asdf install python
+asdf install
+
+# install cfn-lint
+pip install --user cfn-lint
